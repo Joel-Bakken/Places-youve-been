e3c4e188b3459077088f5ca6d1cc17cceb453b33
Joel Bakken and Laura Hamilton <joel@>
2017-06-12 14:19:59 -0700

    add all the objects and fixed css styling, and fixed basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,44 @@
 //business logic
+function Vacation2(place, date, landmark, food) {
+  this.placeName = place;
+  this.dateName = date;
+  this.landmarkName = landmark;
+  this.foodName = food;
+}
 
+Vacation2.prototype.experiment = function() {
+  return this.placeName + " " + this.dateName;
+}
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("form#new-place").submit(function(event) {
     event.preventDefault();
-  })
-});
+
+    var inputtedNewPlace = $("input#newPlace").val();
+    var inputtedNewDate = $("input#newDate").val();
+    var inputtedNewLandmark = $("input#newLandmark").val();
+    var inputtedNewFood = $("input#newFood").val();
+
+    var newVacation = new Vacation2(inputtedNewPlace, inputtedNewDate, inputtedNewLandmark, inputtedNewFood);
+
+    $("ul#location").append("<li><span class='bananas'>" +  newVacation.experiment() + "</span></li>");
+
+    $(".bananas").last().click(function() {
+    $("#show-location").show();
+    $("#show-location h2").text(newVacation.placeName);
+    $(".location-name").text(newVacation.placeName);
+    $(".date").text(newVacation.dateName);
+    $(".favorite-landmark").text(newVacation.landmarkName);
+    $(".favorite-food").text(newVacation.foodName);
+    });
+
+    $("input#newPlace").val("");
+    $("input#newDate").val("");
+    $("input#newLandmark").val("");
+    $("input#newFood").val("");
+
+
+
+    });
+  });
